95db981cfb4d16498b223ebc9a433ed6f55c3f55
pedro00876 <pcchagas00@gmail.com>
2025-04-24 17:22:57 -0300

    Adicionando gráfico de radar

--- GrowUp/src/App.jsx
+++ GrowUp/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 
 //Componentes
 import GraficoDeArea from "./components/GraficoDeArea";
+import GraficoDeAreaPolar from "./components/GraficoDeAreaPolar";
 import GraficoDeBarra from "./components/GraficoDeBarra";
 import GraficoDeBolha from "./components/GraficoDeBolha";
 import GraficoDeLinha from "./components/GraficoDeLinha";
 import GraficoDePizza from "./components/GraficoDePizza";
+import GraficoDeRadar from "./components/GraficoDeRadar";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
       <GraficoDeBolha />
       <GraficoDePizza />
       <GraficoDeLinha />
+      <GraficoDeAreaPolar />
+      <GraficoDeRadar />
     </div>
   );
 }
